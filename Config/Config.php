d963fbf97c2645f20073bac2bcb898ff5b5de830
PredoX <edo.arce.e@gmail.com>
2021-11-11 11:00:09 -0300

    Update Config.php
    generacion de conexion principal

--- Config/Config.php
+++ Config/Config.php
@@ -8,3 +8,6 @@ $dotenv->load();
 
 ini_set('default_charset', 'UTF-8');
 date_default_timezone_set('America/Santiago');
+
+# Generar conexion principal
+$_SESSION['conexion'] = new PDO('mysql:host=' . $_ENV['host_db'] . ';dbname=' . $_ENV['name_db'] . ';charset=utf8;', $_ENV['user_db'], $_ENV['pass_db'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
